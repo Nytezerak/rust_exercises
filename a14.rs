32a3fb4ab7d603d33b6de7a3587873b5accc432b
Nytezerak <chrysdragon.97@hotmail.com>
2022-09-09 00:42:55 -0300

    a14 ok

--- a14.rs
+++ a14.rs
@@ -10,5 +10,39 @@
 // * Iterate through the vector using a for..in loop
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
+struct Person{
+    name: String,
+    age: i32,
+    fav_color: String,
+}
 
-fn main() {}
+fn print_info(data: &String) {
+    println!("{:?}", data);
+}
+
+fn main() {
+    let people = vec![
+        Person{
+            name: String::from("Bulbasaur"),
+            age: 5,
+            fav_color: "green".to_owned(),
+        },
+        Person{
+            name: String::from("Ivysaur"),
+            age: 25,
+            fav_color: "blue".to_owned(),
+        },
+        Person{
+            name: String::from("Venusaur"),
+            age: 35,
+            fav_color: "purple".to_owned(),
+        },
+    ];
+
+    for person in people {
+        if person.age >= 20 {
+            print_info(&person.name);
+            print_info(&person.fav_color)
+        }
+    }
+}
